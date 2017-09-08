e85e6e0229c7fd927aa4e0103f1478ddb8ff6ba7
DITO <DITO@192.168.56.1>
2017-09-08 13:04:09 +0700

    MODUL 3 INHERITANSE

---
+++ src/latihan1/pertama.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author DITO
+ */
+public class pertama {
+    private final int a = 10;
+    
+    protected void terprotek ()
+    {
+            System.out.println("Method ini hanya untuk Anaknya");
+    }
+    public void info ()
+    {
+        System.out.println ("a = "+a);
+        System.out.println ("Dipanggil pada = "+this.getClass ().getName());
+    }
+}
